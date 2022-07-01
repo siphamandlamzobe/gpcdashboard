9d4cd3371c5cac92ec6d00944395465423820f76
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-07-01 14:44:22 +0200

    updated the packages and the App.css

--- source/react-js-client/src/App.js
+++ source/react-js-client/src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import New from "./pages/new/New";
+import List from "./pages/list/List";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="serviceReport">
+            <Route index element={<List />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
