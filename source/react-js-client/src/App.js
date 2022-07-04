ee5ee536f36f703f7ddeed417546423c966ecf06
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-07-04 13:52:01 +0200

    added the dummy data

--- source/react-js-client/src/App.js
+++ source/react-js-client/src/App.js
@@ -1,9 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/home/Home";
 import New from "./pages/new/New";
 import List from "./pages/list/List";
 
 function App() {
+  const DUMMY_REPORTS = [
+    {
+      id: "e1",
+      attendance: "10",
+      serviceType: "Sunday",
+      serviceDate: new Date(2022, 5, 3),
+      serviceReview:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+      id: "e2",
+      attendance: "13",
+      serviceType: "Wednesday",
+      serviceDate: new Date(2022, 5, 4),
+      serviceReview:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+      id: "e3",
+      attendance: "100",
+      serviceType: "Wednesday",
+      serviceDate: new Date(2022, 6, 4),
+      serviceReview:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+  ];
+
+  const [serviceReports, setServiceReports] = useState(DUMMY_REPORTS);
+  const onAddServiceReportHandler = (report) => {
+    setServiceReports((prevReports) => {
+      return [report, ...prevReports];
+    });
+  };
   return (
     <div className="App">
       <BrowserRouter>
@@ -11,9 +45,12 @@ function App() {
           <Route path="/">
             <Route index element={<Home />} />
           </Route>
-          <Route path="serviceReport">
-            <Route index element={<List />} />
-            <Route path="new" element={<New />} />
+          <Route path="serviceReports">
+            <Route index element={<List serviceReports={serviceReports} />} />
+            <Route
+              path="new"
+              element={<New onAddServiceReport={onAddServiceReportHandler} />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
