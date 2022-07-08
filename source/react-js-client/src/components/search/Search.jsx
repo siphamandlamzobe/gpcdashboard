22c96a810bffd4f613a45c523ebef854580dd208
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-07-08 13:47:47 +0200

    moved the search into the serviceReports component.

---
+++ source/react-js-client/src/components/search/Search.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Search = (props) => {
+  const searchHandler = (e) => {
+    props.getSearchKeyword(e.target.value);
+  };
+  return (
+    <form className="w-3/4 relative">
+      <svg
+        width="20"
+        height="20"
+        fill="currentColor"
+        className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-green-500"
+        aria-hidden="true"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+        />
+      </svg>
+      <input
+        className="focus:ring-2 focus:ring-green-400 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
+        type="text"
+        placeholder="Search Service Reports"
+        onChange={searchHandler}
+      />
+    </form>
+  );
+};
+
+export default Search;
--- source/react-js-client/src/pages/list/List.jsx
+++ source/react-js-client/src/pages/list/List.jsx
@@ -3,31 +3,36 @@ import ServiceReports from "../../components/serviceReport/ServiceReports";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
+import Search from "../../components/search/Search";
 
 const List = (props) => {
-  const getQuery = (query) =>{
+  const getQuery = (query) => {
     props.searchTerm(query);
-  }
+  };
   return (
     <div className="flex w-full h-screen">
       <Sidebar />
 
       <div className="flex-auto">
-        <Navbar getSearchKeyword={getQuery}/>
+        <Navbar />
         <div className="flex p-4 items-center m-8 max-w-[80%] mx-auto w-auto shadow-3xl">
-          <div className="flex w-full text-2xl m-2 justify-between bg-white text-gray-500">
-            Add New Service Report
+          <div className="flex w-full text-2xl m-2 justify-between text-gray-500">
+            <Search getSearchKeyword={getQuery} />
             <Link
               to="/serviceReports/new"
               style={{ textDecoration: "none" }}
               className="text-lg font-[400] border-2 border-green-600 rounded-md p-1 no-underline cursor-pointer"
             >
-              Add New
+              Add New Report
             </Link>
           </div>
         </div>
 
-        <ServiceReports serviceReports={props.serviceReports} onEditHandler={props.onEditHandler} onDeleteHandler={props.onDeleteHandler}/>
+        <ServiceReports
+          serviceReports={props.serviceReports}
+          onEditHandler={props.onEditHandler}
+          onDeleteHandler={props.onDeleteHandler}
+        />
       </div>
     </div>
   );
