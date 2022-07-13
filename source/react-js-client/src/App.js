4dcf7a610693b971f4780270a7115985e82f8873
Siphamandla Mzobe <corneliusmzobe@gmail.com>
2022-07-13 19:38:56 +0200

    removed the side and navbar from all the pages.

--- source/react-js-client/src/App.js
+++ source/react-js-client/src/App.js
@@ -1,22 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 import PageNotFound from "./components/PageNotFound";
+import Sidebar from "./components/sidebar/Sidebar";
 import { Edit, Home, List, New } from "./pages/index";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-          </Route>
-          <Route path="serviceReports">
-            <Route index element={<List />} />
-            <Route path="new" element={<New />} />
-            <Route path="edit/:id" element={<Edit />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <div className="flex">
+          <Sidebar />
+          <div className="flex-auto">
+            <Navbar />
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+              </Route>
+              <Route path="serviceReports">
+                <Route index element={<List />} />
+                <Route path="new" element={<New />} />
+                <Route path="edit/:id" element={<Edit />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </div>
+        </div>
       </BrowserRouter>
     </div>
   );
--- source/react-js-client/src/pages/edit/Edit.jsx
+++ source/react-js-client/src/pages/edit/Edit.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Navbar from "../../components/navbar/Navbar";
-import Sidebar from "../../components/sidebar/Sidebar";
 import ServiceReportEditForm from "./ServiceReportEditForm";
 import api from "../../api/serviceReports";
 
@@ -14,10 +12,7 @@ const Edit = () => {
   };
   return (
     <div className="flex">
-      <Sidebar />
       <div className="flex-auto">
-        <Navbar />
-
         <div className="flex p-4 items-center m-8 max-w-[80%] mx-auto w-auto shadow-3xl">
           <div className="flex w-full text-2xl m-2 justify-between bg-white text-gray-500">
             Edit Service Report
