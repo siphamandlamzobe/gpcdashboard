d2e85350b70a43662e3047f536e6e39b0a2e2a13
Siphamandla Mzobe <corneliusmmzobe@gmail.com>
2022-08-16 12:46:07 +0200

    API001 added the api project and initial files

---
+++ source/gpcapi/GPCApi/Controllers/ServiceReportController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPCApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ServiceReportController : ControllerBase
+{
+    
+}
---
+++ source/gpcapi/GPCApi/Models/ServiceReport.cs
@@ -0,0 +1,16 @@
+namespace GPCApi.Models;
+
+public class ServiceReportDto
+{
+    public int id { get; set; }
+    public int attandance { get; set; }
+    public int firsttimers { get; set; }
+    public int soulsSaved { get; set; }
+    public string? serviceType { get; set; }
+    public DateTime serviceDate { get; set; }
+    public string serviceReview { get; set; }
+    public DateTime createdOn { get; set; }
+    public DateTime updatedOn { get; set; }
+}
+
+
