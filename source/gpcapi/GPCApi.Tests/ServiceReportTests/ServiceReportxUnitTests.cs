941384c92bfe16f4d5181ceab1264b8ff68c1e7c
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-08-19 09:39:05 +0200

    API002 added the GIVEN_ServiceReportId_WHEN_GettingServiceReportById_RETURN_NotFound test

---
+++ source/gpcapi/GPCApi.Tests/ServiceReportTests/ServiceReportxUnitTests.cs
@@ -0,0 +1,95 @@
+using GPCApi.Controllers;
+using GPCApi.Repository;
+using GPCApi.Repository.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Core;
+using Moq;
+
+namespace GPCApi.Tests
+{
+    public class ServiceReportxUnitTests
+    {
+        [Fact]
+        public async Task GIVEN_GetAllEndPoint_WHEN_GettingAllServiceReports_RETURN_CorrectCount()
+        {
+            //Arrange
+            var mockServiceReportRepository = new Mock<IServiceReportRepository>();
+            mockServiceReportRepository.Setup(repo => repo.GetAll()).ReturnsAsync(GetTestServiceReports);
+            var serviceReportController = new ServiceReportController(mockServiceReportRepository.Object);
+
+            //Act
+            var result = await serviceReportController.GetServiceReports();
+
+            // Assert
+            var okObjectResult = Assert.IsType<OkObjectResult>(result);
+            var okObjectResultValueCount = (int)okObjectResult.Value.GetType().GetProperty("Count").GetValue(okObjectResult.Value);
+            Assert.Equal(2, okObjectResultValueCount);
+        }
+
+        [Fact]
+        public async Task GIVEN_ServiceReportId_WHEN_GettingServiceReportById_RETURN_ServiceReport()
+        {
+            //Arrange
+            int serviceReportId = 1;
+            Mock<IServiceReportRepository>? mockServiceReportRepository = new Mock<IServiceReportRepository>();
+            mockServiceReportRepository.Setup(repo => repo.GetById(serviceReportId)).ReturnsAsync(GetTestServiceReports().FirstOrDefault(s => s.Id == serviceReportId));
+            var serviceReportController = new ServiceReportController(mockServiceReportRepository.Object);
+
+            //Act
+            var result = await serviceReportController.GetServiceReportById(serviceReportId);
+
+            // Assert
+            var okObjectResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Sunday", okObjectResult.Value.GetType().GetProperty("ServiceType").GetValue(okObjectResult.Value));
+            Assert.Equal(12322, okObjectResult.Value.GetType().GetProperty("Attendance").GetValue(okObjectResult.Value));
+            Assert.Equal(23232, okObjectResult.Value.GetType().GetProperty("SoulsSaved").GetValue(okObjectResult.Value));
+        }
+
+        [Fact]
+        public async Task GIVEN_ServiceReportId_WHEN_GettingServiceReportById_RETURN_NotFound()
+        {
+            //Arrange
+            int serviceReportId = 1;
+            Mock<IServiceReportRepository>? mockServiceReportRepository = new Mock<IServiceReportRepository>();
+            mockServiceReportRepository.Setup(repo => repo.GetById(serviceReportId)).ReturnsAsync((ServiceReport)null);
+            var serviceReportController = new ServiceReportController(mockServiceReportRepository.Object);
+
+            //Act
+            var result = await serviceReportController.GetServiceReportById(serviceReportId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        private IEnumerable<ServiceReport> GetTestServiceReports()
+        {
+            var serviceReports = new List<ServiceReport>();
+            serviceReports.Add(new ServiceReport()
+            {
+                ServiceDate = new DateTime(2022, 8, 18),
+                Id = 1,
+                ServiceType = "Sunday",
+                ServiceReview = "This is the review.",
+                Attendance = 12322,
+                CreatedOn = new DateTime(2022, 8, 18),
+                SoulsSaved = 23232,
+                Firsttimers = 4232
+            });
+
+            serviceReports.Add(new ServiceReport()
+            {
+                ServiceDate = new DateTime(2022, 6, 20),
+                Id = 2,
+                ServiceType = "Wednesday",
+                ServiceReview = "This is the review.",
+                Attendance = 12323232,
+                CreatedOn = new DateTime(2022, 6, 20),
+                SoulsSaved = 3343,
+                Firsttimers = 433
+            });
+
+            return serviceReports;
+        }
+
+    }
+}
