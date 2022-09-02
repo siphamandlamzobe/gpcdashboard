2cdadb932d31861c521d938ed4a10a7b8e45554e
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-09-02 14:09:51 +0200

    API005 removed the parameters on repo methods.

--- source/gpcapi/GPCApi.Repository/Repositories/ServiceReport/ServiceReportRepository.cs
+++ source/gpcapi/GPCApi.Repository/Repositories/ServiceReport/ServiceReportRepository.cs
@@ -16,55 +16,26 @@ public class ServiceReportRepository : IServiceReportRepository
 
     public Task<int> Add(ServiceReport serviceReport)
     {
-        var parameters = new DynamicParameters();
-        parameters.Add("@attendance", serviceReport.Attendance);
-        parameters.Add("@firsttimers", serviceReport.Firsttimers);
-        parameters.Add("@serviceType", serviceReport.ServiceType);
-        parameters.Add("@soulsSaved", serviceReport.SoulsSaved);
-        parameters.Add("@serviceDate", serviceReport.ServiceDate);
-        parameters.Add("@serviceReview", serviceReport.ServiceReview);
-        parameters.Add("@createdOn", DateTime.Now);
-
-        return _dbConnection.QuerySingleAsync<int>("AddServiceReport", parameters, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
+        return _dbConnection.QuerySingleAsync<int>("AddServiceReport", serviceReport, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
     }
 
     public Task Delete(int id)
     {
-        var parameter = new DynamicParameters();
-        parameter.Add("@id", id);
-        return _dbConnection.ExecuteAsync("DeleteServiceReport", parameter, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
+        return _dbConnection.ExecuteAsync("DeleteServiceReport", new { id = @id }, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
     }
 
-    public Task<IEnumerable<ServiceReport>> GetAll()
+    public async Task<IEnumerable<ServiceReport>> GetAll()
     {
-        return _dbConnection.QueryAsync<ServiceReport>("GetAllServiceReports", null, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
+        return await _dbConnection.QueryAsync<ServiceReport>("GetAllServiceReports", null, null, CommandTimeout, CommandType.StoredProcedure);
     }
 
     public Task<ServiceReport> GetById(int id)
     {
-        var parameter = new DynamicParameters();
-        parameter.Add("@id", id);
-        return _dbConnection.QuerySingleAsync<ServiceReport>("GetServiceReportById", parameter, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
-    }
-
-    public Task<IEnumerable<ServiceReport>> Search(string query)
-    {
-        var parameter = new DynamicParameters();
-        parameter.Add("@query", query);
-        return _dbConnection.QueryAsync<ServiceReport>("SearchServiceReports", parameter, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
+        return _dbConnection.QuerySingleAsync<ServiceReport>("GetServiceReportById", new { id = @id }, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
     }
 
     public Task Update(ServiceReport serviceReport)
     {
-        var parameters = new DynamicParameters();
-        parameters.Add("@id", serviceReport.Id);
-        parameters.Add("@attendance", serviceReport.Attendance);
-        parameters.Add("@firsttimers", serviceReport.Firsttimers);
-        parameters.Add("@serviceType", serviceReport.ServiceType);
-        parameters.Add("@soulsSaved", serviceReport.SoulsSaved);
-        parameters.Add("@serviceDate", serviceReport.ServiceDate);
-        parameters.Add("@serviceReview", serviceReport.ServiceReview);
-
-        return _dbConnection.ExecuteAsync("UpdateServiceReport", parameters, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
+        return _dbConnection.ExecuteAsync("UpdateServiceReport", serviceReport, null, commandTimeout: CommandTimeout, commandType: CommandType.StoredProcedure);
     }
 }
