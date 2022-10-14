70066e27c7607583a06e33cc68c1b9b666d19f5a
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-10-14 11:41:59 +0200

    API1014 added get all service reports proc

---
+++ source/gpcapi-db-migrator/DbMigrator/Add_Proc_GetAllServiceReports.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace DbMigrator;
+
+[Migration(202210141134)]
+public class Add_Proc_GetAllServiceReports : Migration
+{
+    public override void Down()
+    {
+    }
+
+    public override void Up()
+    {
+        Execute.Script("Add_Proc_GetAllServiceReports.sql");
+    }
+}
---
+++ source/gpcapi-db-migrator/DbMigrator/Add_Proc_GetAllServiceReports.sql
@@ -0,0 +1,16 @@
+﻿CREATE OR ALTER  PROCEDURE [dbo].GetAllServiceReports
+
+AS
+BEGIN
+	SELECT [id]
+      ,[serviceTypeId]
+      ,[attendance]
+      ,[firsttimers]
+      ,[soulsSaved]
+      ,[serviceReview]
+      ,[serviceDate]
+      ,[createdOn]
+      ,[updatedOn]
+  FROM [dbo].[ServiceReport] ORDER BY 1 DESC 
+END
+GO
