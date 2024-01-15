b20be0e4c443a08c772d3d15b9e0f37fe8bca5cb
siphamandlamzobe <corneliusmzobe@gmail.com>
2024-01-15 12:07:59 +0200

    added a home endpoint

---
+++ source/gpcapi/GPCApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GPCApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetServiceReports()
+        {
+            return Ok(new { message = "live..."});
+        }
+    }
+}
